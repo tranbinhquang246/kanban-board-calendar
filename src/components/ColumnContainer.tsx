b15884912d937140b9878b56a7a7b358c2b89c40
Quang Tran <quangtrangameagelayer@gmail.com>
2023-09-08 14:13:19 +0700

    Implement logic date

--- src/components/ColumnContainer.tsx
+++ src/components/ColumnContainer.tsx
@@ -6,57 +6,59 @@ import PlusIcon from "../icons/PlusIcon";
 import TaskCard from "./TaskCard";
 
 interface Props {
-    column: Column;
-    deleteColumn: (id: Id) => void;
-    updateColumn: (id: Id, title: string) => void;
+  column: Column;
+  deleteColumn: (id: Id) => void;
+  updateColumn: (id: Id, title: string) => void;
 
-    createTask: (columnId: Id) => void;
-    updateTask: (id: Id, content: string) => void;
-    deleteTask: (id: Id) => void;
-    tasks: Task[];
+  createTask: (columnId: Id) => void;
+  updateTask: (id: Id, content: string) => void;
+  deleteTask: (id: Id) => void;
+  tasks: Task[];
+  className?: String;
 }
 
 function ColumnContainer({
-    column,
-    updateColumn,
-    createTask,
-    tasks,
-    deleteTask,
-    updateTask,
+  column,
+  updateColumn,
+  createTask,
+  tasks,
+  deleteTask,
+  updateTask,
+  className,
 }: Props) {
-    const [editMode, setEditMode] = useState(false);
+  const [editMode, setEditMode] = useState(false);
 
-    const tasksIds = useMemo(() => {
-        return tasks.map((task) => task.id);
-    }, [tasks]);
+  const tasksIds = useMemo(() => {
+    return tasks.map((task) => task.id);
+  }, [tasks]);
 
-    const {
-        setNodeRef,
-        attributes,
-        listeners,
-        transform,
-        transition,
-        isDragging,
-    } = useSortable({
-        id: column.id,
-        data: {
-            type: "Column",
-            column,
-        },
-        disabled: editMode,
-    });
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: column.id,
+    data: {
+      type: "Column",
+      column,
+    },
+    disabled: editMode,
+  });
 
-    const style = {
-        transition,
-        transform: CSS.Transform.toString(transform),
-    };
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
 
-    if (isDragging) {
-        return (
-            <div
-                ref={setNodeRef}
-                style={style}
-                className="
+  if (isDragging) {
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        className="
       bg-gray-50
       opacity-40
       border-2
@@ -67,33 +69,24 @@ function ColumnContainer({
       flex
       flex-col
       "
-            ></div>
-        );
-    }
+      ></div>
+    );
+  }
 
-    return (
-        <div
-            ref={setNodeRef}
-            style={style}
-            className="
-  bg-gray-50
-  w-[150px]
-  h-[250px]
-  max-h-[250px]
-  border-[2px]
-  border-gray-500
-  flex
-  flex-col
-  "
-        >
-            {/* Column title */}
-            <div
-                {...attributes}
-                {...listeners}
-                onClick={() => {
-                    setEditMode(true);
-                }}
-                className="
+  return (
+    <div
+      ref={setNodeRef}
+      style={style}
+      className={`bg-gray-50 w-[150px] h-[250px] max-h-[250px] border-[2px] border-gray-500 flex flex-col ${className}`}
+    >
+      {/* Column title */}
+      <div
+        {...attributes}
+        {...listeners}
+        onClick={() => {
+          setEditMode(true);
+        }}
+        className="
       bg-white
       text-md
       h-[30px]
@@ -109,38 +102,38 @@ function ColumnContainer({
       items-center
       justify-center
       "
-            >
-                <div className="flex gap-2   text-center justify-center">
-                    {!editMode && column.title}
-                    {editMode && <p className="w-full text-center">{column.title}</p>}
-                </div>
-            </div>
-
-            {/* Column task container */}
-            <div className="flex flex-grow flex-col gap-2 p-2 overflow-x-hidden overflow-y-auto">
-                <SortableContext items={tasksIds}>
-                    {tasks.map((task) => (
-                        <TaskCard
-                            key={task.id}
-                            task={task}
-                            deleteTask={deleteTask}
-                            updateTask={updateTask}
-                        />
-                    ))}
-                </SortableContext>
-            </div>
-            {/* Column footer */}
-            {/* <button
-                className="flex gap-2 items-center border-columnBackgroundColor  border px-4 border-x-columnBackgroundColor  text-rose-500"
-                onClick={() => {
-                    createTask(column.id);
-                }}
-            >
-                <PlusIcon />
-                Add task
-            </button> */}
+      >
+        <div className="flex gap-2   text-center justify-center">
+          {!editMode && column.title}
+          {editMode && <p className="w-full text-center">{column.title}</p>}
         </div>
-    );
+      </div>
+
+      {/* Column task container */}
+      <div className="flex flex-grow flex-col gap-2 p-2 overflow-x-hidden overflow-y-auto">
+        <SortableContext items={tasksIds}>
+          {tasks.map((task) => (
+            <TaskCard
+              key={task.id}
+              task={task}
+              deleteTask={deleteTask}
+              updateTask={updateTask}
+            />
+          ))}
+        </SortableContext>
+      </div>
+      {/* Column footer */}
+      {/* <button
+        className="flex gap-2 items-center border-columnBackgroundColor  border px-4 border-x-columnBackgroundColor  text-rose-500"
+        onClick={() => {
+          createTask(column.id);
+        }}
+      >
+        <PlusIcon />
+        Add task
+      </button> */}
+    </div>
+  );
 }
 
 export default ColumnContainer;
--- src/components/utils/index.ts
+++ src/components/utils/index.ts
@@ -1,28 +1,19 @@
-export  const getNumberOfDaysInCurrentMonth = ()  =>{
-    // Tạo một đối tượng Date hiện tại
-    const currentDate = new Date();
-  
-    // Lấy tháng hiện tại
-    const currentMonth = currentDate.getMonth();
-  
-    // Đặt ngày trong tháng thành 0 để lấy ngày cuối cùng của tháng trước đó (tháng hiện tại)
-    currentDate.setDate(0);
-  
-    // Lấy số ngày trong tháng hiện tại
-    const numberOfDaysInCurrentMonth = currentDate.getDate();
-  
-    return numberOfDaysInCurrentMonth;
-}
-export const  createArrayOfObjects = (numDays : any)  => {
-  const result = [];
+import { format } from "date-fns";
 
-  for (let i = 0; i < numDays; i++) {
-    const newObj = {
-      id: i,
-      title: i,
-    };
-    result.push(newObj);
-  }
-
+export const createArrayOfObjects = (numDays: any) => {
+  const result: any = [];
+  numDays.map((element: any) => {
+    result.push({
+      id: format(new Date(element), "yyyy/MM/dd"),
+      title:
+        format(new Date(element), "dd") +
+        " Date " +
+        `(${format(new Date(element), "EEE")})`,
+    });
+  });
   return result;
-}
+};
+
+export const flattenArray = (array: any) => {
+  return [].concat(...array);
+};
